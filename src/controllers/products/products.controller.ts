20b7821eeb1d32078410ee645e11eb7ffaea8293
Alexander Herrera <chander.zap@gmail.com>
2021-06-02 20:39:26 -0500

    Delete Put and Post methods

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,18 +1,51 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
   @Get(':id')
-  getOneProduct(@Param('id') id: string) {
+  getOne(@Param('id') id: string) {
     return `product ${id}`;
   }
 
   @Get()
-  getProducts(
+  getAll(
     @Query('limit') limit: number,
     @Query('offset') offset: number,
     @Query('brand') brand: string,
   ) {
     return `Limit: ${limit}, offset: ${offset}, Brand: ${brand}`;
+  }
+
+  @Post()
+  create(@Body() payload: any) {
+    return {
+      Message: 'Created',
+      Product: payload,
+    };
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: any) {
+    payload.id = id;
+    return {
+      Message: 'Created',
+      Updated: payload,
+    };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return {
+      Message: `Product ${id} created`,
+    };
   }
 }
