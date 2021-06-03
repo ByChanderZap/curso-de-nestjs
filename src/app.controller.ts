6b25a3f117616facb0d4c9fd155972541e99ee22
Alexander Herrera <chander.zap@gmail.com>
2021-06-02 19:26:14 -0500

    How to add params

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,21 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('new')
+  newEndpoint() {
+    return 'im new'
+  }
+
+  @Get('products/:id')
+  getOneProduct(@Param('id') id: string) {
+
+    return `product ${id}`
+  }
+
+  @Get('categories/:categoryId/products/:productId')
+  getCategory(@Param('categoryId') categoyId: string, @Param('productId') productId: string) {
+    return `Product: ${productId} category: ${categoyId}`
   }
 }
