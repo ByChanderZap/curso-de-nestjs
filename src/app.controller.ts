e0c092d777fa63a11fbdd810b8e95d7751b0a3b2
Alexander Herrera <chander.zap@gmail.com>
2021-06-02 20:16:58 -0500

    Modulating code and added missing controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,27 +8,5 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Get('products/:id')
-  getOneProduct(@Param('id') id: string) {
-    return `product ${id}`;
-  }
-
-  @Get('products')
-  getProducts(
-    @Query('limit') limit: number,
-    @Query('offset') offset: number,
-    @Query('brand') brand: string,
-  ) {
-    return `Limit: ${limit}, offset: ${offset}, Brand: ${brand}`;
-  }
-
-  @Get('categories/:categoryId/products/:productId')
-  getCategory(
-    @Param('categoryId') categoyId: string,
-    @Param('productId') productId: string,
-  ) {
-    return `Product: ${productId} category: ${categoyId}`;
   }
 }
--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  @Get(':id')
+  getOneProduct(@Param('id') id: string) {
+    return `product ${id}`;
+  }
+
+  @Get()
+  getProducts(
+    @Query('limit') limit: number,
+    @Query('offset') offset: number,
+    @Query('brand') brand: string,
+  ) {
+    return `Limit: ${limit}, offset: ${offset}, Brand: ${brand}`;
+  }
+}
